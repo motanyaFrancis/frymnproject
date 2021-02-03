fa065334f6d3aed6f02b7176f7b6e2f00ebf79ec
Francis motanya <58334093+motanyaFrancis@users.noreply.github.com>
2021-02-03 12:58:39 +0300

    'message'

--- pharmacy/settings/base.py
+++ pharmacy/settings/base.py
@@ -12,11 +12,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
-from django.conf import settings
-
-
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -52,12 +47,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-settings.configure(
-    ROOT_URLCONF = 'pharmacy.urls'
-
-    ),
-)
-
+ROOT_URLCONF = 'pharmacy.urls'
 
 TEMPLATES = [
     {
